810f055d2ed1b87bbeec98ee46a6248d3b097eb9
d.luca727 <d.luca@c.lutech.it>
2022-09-22 12:16:25 +0200

    db deployment

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-use secrecy::ExposeSecret;
-use sqlx::postgres::{PgPool, PgPoolOptions};
+use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
@@ -13,7 +12,7 @@ async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPoolOptions::new()
         .connect_timeout(std::time::Duration::from_secs(2))
-        .connect_lazy_with(configuration.database.without_db());
+        .connect_lazy_with(configuration.database.with_db());
 
     let address = format!(
         "{}:{}",
